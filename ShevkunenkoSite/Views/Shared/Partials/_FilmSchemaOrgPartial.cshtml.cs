83f4dab8e0263574e7de80df735898fde5045b8a
Vladimir-Zakharenkov <zakharenkov-1965@yandex.ru>
2026-09-09 23:46:25 +0300

    09-09-2026-23-46

---
+++ ShevkunenkoSite/Views/Shared/Partials/_FilmSchemaOrgPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShevkunenkoSite.Views.Shared.Partials
+{
+    public class _FilmSchemaOrgPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
